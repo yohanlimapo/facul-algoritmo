cc371c7bc5713bfccb15a5d1a799e2d3c131d37e
yohanlimapo <yohan.limapo@gmail.com>
2024-05-14 17:14:43 -0300

    fim da aula 8

--- Aula 2.8/desafio.py
+++ Aula 2.8/desafio.py
@@ -1,26 +1,59 @@
-op_validos = ['+', '-', '*','/']
+# op_validos = ['+', '-', '*','/']
 
-while True:
-    e = input("Digite o valor a esquerda do operador: ")
-    if e.isdigit():
-        op = input(f'digite o operador. Opções: {op_validos} ')
-    else:
-        print("Número inválido!")    
-        continue
-    d = input("Digite o valor a direita do operador: ")
-    if d.isdigit():
-        if op in op_validos:
-            if op == '/' and d == '0':
-                print("Divisão por zero! Tente novamente.")
-                continue
-            expressao = e + ' ' + op + ' ' + d
-            print(e, op, d, "=", eval(expressao))
-            break
-        print("Valores ou operador incorreto!")
-    else: 
-        print("Número inválido!")
+# while True:
+#     e = input("Digite o valor a esquerda do operador: ")
+#     if e.isdigit():
+#         op = input(f'digite o operador. Opções: {op_validos} ')
+#     else:
+#         print("Número inválido!")    
+#         continue
+#     d = input("Digite o valor a direita do operador: ")
+#     if d.isdigit():
+#         if op in op_validos:
+#             if op == '/' and d == '0':
+#                 print("Divisão por zero! Tente novamente.")
+#                 continue
+#             expressao = e + ' ' + op + ' ' + d
+#             print(e, op, d, "=", eval(expressao))
+#             break
+#         print("Valores ou operador incorreto!")
+#     else: 
+#         print("Número inválido!")
 
 # ​​​​​​​​​​​​​​Com base nessas informações, responda:
 # A) Qual função do Python poderia ser usada para verificar se o que foi digitado, para o valor que vai a esquerda ou a direita do operador, é um valor numérico?
 # B) Como ficaria a expressão lógica que está sendo avaliada pela instrução if?
 
+# dicionario = {
+
+# 'Love': 'Amor',
+
+# 'Paz': 'Peace',
+
+# 5: 'Five',
+
+# True: 'True',
+
+# False: 'Falso'
+
+# }
+
+# print( dicionario[ True ] )
+
+# a = (1, 2, 3)
+
+# b = [2, 3, 4]
+
+# c = {"1": 1, "2": 2, "3": 3}
+
+ 
+# print(a[2], b[2], c["2"])
+
+
+# ( ( a + ( b * ( c * * d ) ) / e ) - f )
+
+# ( ( ( a + b ) * ( c * * d ) ) / e ) ) - f )
+
+# ( ( a + ( ( b * ( ( c * * d ) ) / ( e - f ) )
+ 
+# ( ( a + ( ( b * ( c ** d ) ) / e ) ) - f )
