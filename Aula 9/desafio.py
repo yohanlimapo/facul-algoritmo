3847338bca44abc28b27f4b1469e420711973058
yohanlimapo <yohan.limapo@gmail.com>
2024-05-15 18:09:48 -0300

    Correção para aceitar apenas letra na pergunta de sim e não

--- Aula 9/desafio.py
+++ Aula 9/desafio.py
@@ -26,56 +26,60 @@ TEMP_EXT = float(input("Qual a temperatura da parte EXTERNA do forno?\t"))
 TEMP_INT = float(input("Qual a temperatura INTERNA do fogo?\t"))
 
 clima = input("Estamos no inverno? (S/N)\t")
-if clima == "S" or "s":
-    print("Iniciando desumidificação...")
+if clima.isalpha():
+    if clima == "S" or "s":
+        print("Iniciando desumidificação...")
+        time.sleep(3)
+        # Procedimento de DESUMIDIFICAÇÃO
+        print("Início da desumidificação.")
+        time.sleep(3)
+        # Condicional para TEMPERATURA
+        if TEMP_INT > 100: print ("Forno superaquecido, desligue agora!!!")
+        elif TEMP_INT > 15 and UMIDADE <= 40:
+            print("Acionar exaustor")
+            EXAUSTOR = True
+            time.sleep(2)
+            print ("Exaustor ligado")
+            time.sleep(5)
+            
+            UMIDADE = 5
+            EXAUSTOR = False
+            print("Desumidificação concluída.")
+        else:
+            print("Acionar aquecimento do forno e exaustor do ar.") #TEMP_INT < 15 and UMIDADE >= 40
+            AQUECIMENTO = float(input("Qual a temperatura desejada?\t"))
+            time.sleep(1)
+            print("Desumidificação iniciada.")
+            time.sleep(15)
+            print("Desumidificação concluída.")
+
+    # Sem inverno, sem desumidificação
+    else: print("Pronto para a cocção")
     time.sleep(3)
-    # Procedimento de DESUMIDIFICAÇÃO
-    print("Início da desumidificação.")
+    # Procedimento de COCÇÃO
+    print("Iniciando cocção...")
     time.sleep(3)
-    # Condicional para TEMPERATURA
-    if TEMP_INT > 100: print ("Forno superaquecido, desligue agora!!!")
-    elif TEMP_INT > 15 and UMIDADE <= 40:
-        print("Acionar exaustor")
-        EXAUSTOR = True
-        time.sleep(2)
-        print ("Exaustor ligado")
+    if UMIDADE > 15:
+        print("Acionar exaustor.")
         time.sleep(5)
-        
+
+        if TEMP_INT < 200:
+            print("Acionar aquecimento para a temperatura de 380ºC")
+            time.sleep(5)
+            AQUECIMENTO = 380
+            print("Temperatura atual: 380ºC")
+            time.sleep(1)
         UMIDADE = 5
-        EXAUSTOR = False
-        print("Desumidificação concluída.")
+        print("Exaustor desligado.")
+        time.sleep(2)
+    BOTAO = input("Inserir conteúdo para cocção - quando concluir digitar S\t").upper()
+    if BOTAO.isalpha():
+        if BOTAO == "S":
+            print("Aquecendo, volte em 3 horas.")
+            time.sleep(20)
+        print("Cocção concluída.\nAquecimento desligado.")
     else:
-        print("Acionar aquecimento do forno e exaustor do ar.") #TEMP_INT < 15 and UMIDADE >= 40
-        AQUECIMENTO = float(input("Qual a temperatura desejada?\t"))
-        time.sleep(1)
-        print("Desumidificação iniciada.")
-        time.sleep(15)
-        print("Desumidificação concluída.")
-
-# Sem inverno, sem desumidificação
-else: print("Pronto para a cocção")
-time.sleep(3)
-# Procedimento de COCÇÃO
-print("Iniciando cocção...")
-time.sleep(3)
-if UMIDADE > 15:
-    print("Acionar exaustor.")
-    time.sleep(5)
-
-    if TEMP_INT < 200:
-        print("Acionar aquecimento para a temperatura de 380ºC")
-        time.sleep(5)
-        AQUECIMENTO = 380
-        print("Temperatura atual: 380ºC")
-        time.sleep(1)
-    UMIDADE = 5
-    print("Exaustor desligado.")
-    time.sleep(2)
-
-BOTAO = input("Inserir conteúdo para cocção - quando concluir digitar S").upper()
-if BOTAO == "S":
-    print("Aquecendo, volte em 3 horas.")
-    time.sleep(20)
-print("Cocção concluída.\nAquecimento desligado.")
-
+        print("Precisa digitar 'S'")
+else:
+    print("Precisa digitar 'S'")
 
