667ed75c37fe10db545b1201a501b485bb620d2f
yohanlimapo <yohan.limapo@gmail.com>
2024-05-16 18:58:50 -0300

    verificacao de comando for para contagem de letras

---
+++ Aula 12/estudo.py
@@ -0,0 +1,11 @@
+frase = "Para dizer essa frase, quantas letras eu usei?"
+
+quantidadeDeLetras = 0
+
+for letra in frase:
+    quantidadeDeLetras+=1
+
+print(frase)
+print()
+print("A frase possui",(quantidadeDeLetras),"letras.")
+# No caso ele fez a contagem de CARACTERES e não de letras.
